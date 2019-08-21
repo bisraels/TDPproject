ab4437ad88b5bec27116c472e199d09cc4f0c102
Claire <calbrec2@uoregon.edu>
2019-08-21 13:32:04 -0700

    updates to conditional prob calcs

--- evalCondProb_2state.m
+++ evalCondProb_2state.m
@@ -15,10 +15,10 @@ syms t
 k12 = 1/4;
 k21 = 1/2;
 
-P11(t) = subs(P11)
-P12(t) = subs(P12)
-P21(t) = subs(P21)
-P22(t) = subs(P22)
+P11(t) = subs(P11);
+P12(t) = subs(P12);
+P21(t) = subs(P21);
+P22(t) = subs(P22);
 
 
 
