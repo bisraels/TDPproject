7ca86f9574ad481c6689a58fc8f773a808b4244a
Claire <calbrec2@uoregon.edu>
2019-08-21 15:12:46 -0700

    attempt at C2 calculation in evalCondProb

--- evalCondProb_4state0123.m
+++ evalCondProb_4state0123.m
@@ -69,17 +69,40 @@ P33(t) = vpa(subs(P33));
 %       - Ai values of FRET states
 
 % Two point TCF:
-syms A0 A1 A2 A3
+% syms A0 A1 A2 A3
 
 % This is a vector for the FRET values - assign values
+A0 = 0.34;
+A1 = 0.42;
+A2 = 0.51;
+A3 = 0.67;
+
 A = [A0, A1, A2, A3];
 % Matrix of conditional probabilities
-cP = [P00, P01, P02, P03;...
-    P10, P11, P12, P13;...
-    P20, P21, P22, P23;...
-    P30, P31, P32, P33];
+cP = [P00(t), P01(t), P02(t), P03(t);...
+     P10(t), P11(t), P12(t), P13(t);...
+     P20(t), P21(t), P22(t), P23(t);...
+     P30(t), P31(t), P32(t), P33(t)];
 
+load('sym_prob.mat')
+% This gives the equilibrium probabilties
+% for this state we are using:
+% Pi_Cn4 where i = 0, 1, 2, 3
+ 
+P0_eq = vpa(subs(P0_Cn4));
+P1_eq = vpa(subs(P1_Cn4));
+P2_eq = vpa(subs(P2_Cn4));
+P3_eq = vpa(subs(P3_Cn4));
 
+Peq = [P0_eq; P1_eq; P2_eq; P3_eq];
 
+C2(t) = 0;
+for i = 1:numel(A)
+    for j = 1:numel(A)
+        
+        C2temp(t) = A(j) * cP(j,i) * A(i) * Peq(i);
+        C2(t) = C2(t) + C2temp(t);
+    end
+end
 
 
