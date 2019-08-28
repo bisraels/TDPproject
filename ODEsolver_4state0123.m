89eb6b304d6744ee9702cdacd5167cb6d97a38aa
Claire <calbrec2@uoregon.edu>
2019-08-27 18:00:29 -0700

    today's  TCF stuff

--- ODEsolver_4state0123.m
+++ ODEsolver_4state0123.m
@@ -9,24 +9,15 @@
 %           This is the updated version that eliminates the steps that were
 %           previously giving us complex probabilities
 %
-% MODIFICATIONS:
+% MODIFICATIONS: modified from FourStateODE_0123.m
 %--------------------------------------------------------------------------
 
 clear all
 clc
-
+tic
 syms k01 k02 k03 k10 k12 k13 k20 k21 k23 k30 k31 k32  
 syms P0(t) P1(t) P2(t) P3(t)
-% assume(P0(t),'real')
-% assume(P1(t),'real')
-% assume(P2(t),'real')
-% assume(P3(t),'real')
 
-% Define rate matrix and population vector (as a function of time)
-% K = [-(k01 + k02 + k03), k10, k20, k30;...
-%     k01, -(k10 + k12 + k13), k21, k31;...
-%     k02, k12, -(k20 + k21 + k23), k13;...
-%     k03, k13, k23, -(k30 + k31 + k32);];
 K = [-(k01 + k02 + 0), k10, k20, 0;...
     k01, -(k10 + k12 + 0), k21, 0;...
     k02, k12, -(k20 + k21 + k23), k32;...
@@ -34,14 +25,13 @@ K = [-(k01 + k02 + 0), k10, k20, 0;...
 P(t) = [P0(t);P1(t); P2(t);P3(t)];
 
 % Detailed balance condition
-%k20 = (k10 * k02 * k21)/(k01 * k12);
 k02 = (k01 * k12 * k20)/(k10 * k21);
 
 [Vec, lambda] = eig(K);
-eval0 = real(lambda(1,1));        % eval0 should always be zero.
-eval1 = real(lambda(2,2));
-eval2 = real(lambda(3,3));
-eval3 = real(lambda(4,4));
+eval0 = lambda(1,1);        % eval0 should always be zero.
+eval1 = lambda(2,2);
+eval2 = lambda(3,3);
+eval3 = lambda(4,4);
 
 evec0 = Vec(:,1);
 evec1 = Vec(:,2);
@@ -60,7 +50,6 @@ P01(t) = vpa(Psol_0.P1);
 P02(t) = vpa(Psol_0.P2);
 P03(t) = vpa(Psol_0.P3);
 
-% P0eq = P00(inf);
 
 % Condition 1: P(t) = [P0(t) = 0, P1(t) = 1, P2(t) = 0, P3(t) = 0]
 Psol_1 = dsolve(eqn,[P0(0) == 0 P1(0)==1 P2(0)==0 P3(0)==0]);
@@ -69,7 +58,6 @@ P11(t) = vpa(Psol_1.P1);
 P12(t) = vpa(Psol_1.P2);
 P13(t) = vpa(Psol_1.P3);
 
-% P1eq = P11(inf);
 
 % Condition 2: P(t) = [P0(t) = 0, P1(t) = 0, P2(t) = 1, P3(t) = 0]
 Psol_2 = dsolve(eqn,[P0(0) == 0 P1(0)==0 P2(0)==1 P3(0)==0]);
@@ -78,7 +66,6 @@ P21(t) = vpa(Psol_2.P1);
 P22(t) = vpa(Psol_2.P2);
 P23(t) = vpa(Psol_2.P3);
 
-% P2eq = P22(inf);
 
 % Condition 3: P(t) = [P0(t) = 0, P1(t) = 0, P2(t) = 0, P3(t) = 3]
 Psol_3 = dsolve(eqn,[P0(0) == 0 P1(0)==0 P2(0)==0 P3(0)==1]);
@@ -87,8 +74,7 @@ P31(t) = vpa(Psol_3.P1);
 P32(t) = vpa(Psol_3.P2);
 P33(t) = vpa(Psol_3.P3);
 
-% P3eq = P33(inf);
 
-save('symCondProb_4state0123_fixed.mat','P00','P01','P02','P03','P10','P11','P12','P13','P20','P21','P22','P23','P30','P31','P32','P33','eval0','eval1',)
+save('symCondProb_4state0123_fixed.mat','P00','P01','P02','P03','P10','P11','P12','P13','P20','P21','P22','P23','P30','P31','P32','P33','eval0','eval1','eval2','eval3')
 
-
+toc
