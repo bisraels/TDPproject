3c8081cf6fa8a334668472a1e7efdd4b5e27ffd7
Brett Israels <baimidecho@gmail.com>
2019-11-26 11:35:46 -0800

    new codes

--- P2C_demo.m
+++ P2C_demo.m
@@ -9,22 +9,6 @@ Peq = sym('P%d_eq',[N 1],'positive')  ; %Peq(i) = Pi_eq
 %Make a symbolic row vector of the FRET Values
 A = sym('A%d',[1 N],'positive');         %A(i) = Ai
 
-% Make a symbolic matrix of the conditional probability
-cP = sym('P',[N N]);        %cP(m,n) = Pm_n
-
-% % % Define symbolic matrix of c's for our constants
-% % c = sym('c', [N N]);                    %c(m,n) = cm_n
-% % 
-% % % Define symbolic matrix of the eigenvector components.
-% % % Transpose the matrix for proper matrix mulitplication
-% % v = sym('v',[N N]).';                   %v(m,n) = vn_m
-% % 
-% % % Make a symbolic column vector of the eigenvalues and time
-% % lam = exp(sym('eval',[N 1])*'time');    %lam(i) = exp(evali*time)
-% % 
-% % %make a symbolic figure
-% % p = v*(c.*lam);
-
 % p(:)
 
 %--------------------------------------------------------------------------
@@ -35,6 +19,9 @@ if exist('C2','var')
     clear('C2');
 end
 syms C2;
+% Make a symbolic matrix of the conditional probability
+cP = sym('P',[N N]);        %cP(m,n) = Pm_n
+
 % C2 = [];
 for i = 1:numel(A)
     for j = 1:numel(A)
@@ -54,15 +41,15 @@ end
 % Calculate 2 point TCF (Matrix)
 %--------------------------------------------------------------------------
 
-Amat = A.*eye(N);
-C2test = sum(Amat*cP*Amat*Peq);
-time = time_sim;
-Amat3D = Amat;
-Peq2D = Peq;
-for i = 1:length(time)-1
-    Amat3D = cat(3,Amat3D,Amat);
-    Peq2D(:,i) = cat(Peq2D,Peq);
-end
+% Amat = A.*eye(N);
+% C2test = sum(Amat*cP*Amat*Peq);
+% time = time_sim;
+% Amat3D = Amat;
+% Peq2D = Peq;
+% for i = 1:length(time)-1
+%     Amat3D = cat(3,Amat3D,Amat);
+%     Peq2D(:,i) = cat(Peq2D,Peq);
+% end
 
 % C2 = Amat3D
 %--------------------------------------------------------------------------
@@ -86,3 +73,29 @@ for i = 1:numel(A)
         end
     end
 end
+
+%--------------------------------------------------------------------------
+% Solve for conditional probabilites using the eigen basis
+%--------------------------------------------------------------------------
+
+
+% Define symbolic matrix of c's for our constants
+c = sym('c', [N N]);                    %c(m,n) = cm_n
+
+% Define symbolic matrix of the eigenvector components.
+% Transpose the matrix for proper matrix mulitplication
+v = sym('v',[N N]).';                   %v(m,n) = vn_m
+
+% Make a symbolic column vector of the eigenvalues and time
+lam = exp(sym('eval',[N 1])*'time');    %lam(i) = exp(evali*time)
+
+%make a symbolic figure
+p = v*(c.*lam);
+
+k = sym('k',[N N]).'
+for i = 1:N
+    k(i,i) = 0;
+    k(i,i) = -sum(k(:,i));
+end
+
+
